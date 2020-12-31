e06c4034877a27ba41eddc9ffed9eeb6ebdcdb39
njmarko <marko.njegomir@uns.ac.rs>
2020-12-31 17:13:38 +0100

    Added address class and changed location class. references #4

---
+++ googolplexTX/src/model/Address.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Address {
+	   private Integer number;
+	   private String city;
+	   private Integer zipCode;
+}
--- googolplexTX/src/model/Location.java
+++ googolplexTX/src/model/Location.java
@@ -1,10 +1,9 @@
 package model;
 
 public class Location {
-	   private double longitude;
-	   private double latitude;
-	   private String street;
-	   private int number;
-	   private int city;
-	   private int zipCode;
+	private Double longitude;
+	private Double latitude;
+	private String street;
+	private Address adress;
+	
 }
