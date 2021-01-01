231deb2931f66e00b38ca28c8403648443f8784f
njmarko <marko.njegomir@uns.ac.rs>
2021-01-01 17:59:59 +0100

    Added InMemoryRepository calss that will work with files and data collections. Adjusted the service methods. #5

--- googolplexTX/src/service/implementation/ManifestationDao.java
+++ googolplexTX/src/service/implementation/ManifestationDao.java
@@ -1,56 +1,37 @@
 package service.implementation;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import model.Manifestation;
-import repository.GenericRepository;
-import repository.ManifestationRepository;
+import repository.InMemoryRepository;
 import service.ManifestationService;
 
 public class ManifestationDao implements ManifestationService {
 
-	Map<String, Manifestation> manifestations = new ConcurrentHashMap<String, Manifestation>();
-
-	/*
-	 * TODO consider transfering collections to the repository class
-	 */
-	GenericRepository<Map<String,Manifestation>> repository = new ManifestationRepository();
-	
 	@Override
 	public Collection<Manifestation> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return InMemoryRepository.findAllManifestations();
 	}
 
 	@Override
-	public Manifestation findOne(String id) {
-		return manifestations.getOrDefault(id, null);
+	public Manifestation findOne(String key) {
+		return InMemoryRepository.findOneManifestation(key);
 	}
 
 	@Override
-	public Manifestation save(Manifestation object) {
-		// TODO Auto-generated method stub
-		manifestations.put(object.getId(), object);			
-		repository.save(manifestations);
-		return object;
+	public Manifestation save(Manifestation entity) {
+		InMemoryRepository.save(entity);
+		return entity;
 	}
 
 	@Override
 	public Manifestation delete(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		return InMemoryRepository.deleteManifestation(id);
 	}
 
 	@Override
 	public Manifestation update(Manifestation object) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	public void load() {
-		manifestations = repository.load();
+		return InMemoryRepository.save(object);
 	}
 
 }
