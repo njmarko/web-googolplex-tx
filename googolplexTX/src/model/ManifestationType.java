b0b6ebe7dad72512681b0f71f206c1ed6d98f0b0
njmarko <marko.njegomir@uns.ac.rs>
2021-01-08 14:52:52 +0100

    Added search, sorting and filtering for Manifestations. #19

--- googolplexTX/src/model/ManifestationType.java
+++ googolplexTX/src/model/ManifestationType.java
@@ -41,6 +41,37 @@ public class ManifestationType {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((deleted == null) ? 0 : deleted.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ManifestationType other = (ManifestationType) obj;
+		if (deleted == null) {
+			if (other.deleted != null)
+				return false;
+		} else if (!deleted.equals(other.deleted))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equalsIgnoreCase(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "ManifestationType [name=" + name + ", deleted=" + deleted + "]";
 	}
