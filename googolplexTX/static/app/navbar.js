99609bcde6cf0d5e65ac336e1a296cba486a440a
milospp <misa01523@gmail.com>
2021-02-04 10:43:11 +0100

    Grouped navbar items into dropdown #64

--- googolplexTX/static/app/navbar.js
+++ googolplexTX/static/app/navbar.js
@@ -18,19 +18,54 @@ Vue.component("navbar", {
 				<div class="navbar-nav mr-auto mt-2 mt-lg-0">
 					<a class="navbar-brand" href="/">Googolplex-Tx</a>
 
-					<router-link to="/" class="nav-item nav-link">Home</router-link>
-					<router-link to="/manifestations" class="nav-item nav-link">Manifestations</router-link>
+
 					
-					<router-link v-if="user && user.userRole == 'ADMIN'" to="/users" class="nav-link">Users</router-link> 
-					<router-link v-if="user && user.userRole == 'ADMIN'" to="/add-user" class="nav-link">Add User</router-link>
-					<router-link v-if="user && user.userRole == 'ADMIN'" to="/suspicious-users" class="nav-link">Suspicious Users</router-link>
-					<router-link v-if="user && user.userRole == 'ADMIN'" to="/manifestation-types" class="nav-link">Manif Types</router-link>
-					<router-link v-if="user && user.userRole == 'ADMIN'" to="/customer-types" class="nav-link">Cust Types</router-link>
+					<router-link  v-if="user && user.userRole != 'SALESMAN'" to="/manifestations" class="nav-item nav-link">Manifestations</router-link>
 					
-					<router-link v-if="user && user.userRole == 'SALESMAN'" to="/salesman-add-manif" class="nav-link">Add Manifestation</router-link>
-					<router-link to="/my-manifestations" v-if="user && user.userRole == 'SALESMAN'" class="nav-item nav-link">My Manifestations</router-link>
-					<router-link to="/sold-tickets" v-if="user && user.userRole == 'SALESMAN'" class="nav-item nav-link">Sold Tickets</router-link>
-					<router-link to="/sold-to-users" v-if="user && user.userRole == 'SALESMAN'" class="nav-item nav-link">My Customers</router-link>
+					<li v-if="user && user.userRole == 'ADMIN'" class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuUsers" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						  Users
+						</a>
+						<div class="dropdown-menu dropdown-menu-dark" style="z-index: 150;" aria-labelledby="navbarDropdownMenuUsers">
+							<router-link v-if="user && user.userRole == 'ADMIN'" to="/users" class="nav-link">Users</router-link> 
+							<router-link v-if="user && user.userRole == 'ADMIN'" to="/add-user" class="nav-link">Add User</router-link>
+							<router-link v-if="user && user.userRole == 'ADMIN'" to="/suspicious-users" class="nav-link">Suspicious Users</router-link>
+						</div>
+					</li>
+
+
+					<li v-if="user && user.userRole == 'ADMIN'" class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuTypes" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						  Types
+						</a>
+						<div class="dropdown-menu dropdown-menu-dark" style="z-index: 150;" aria-labelledby="navbarDropdownMenuTypes">
+							<router-link v-if="user && user.userRole == 'ADMIN'" to="/manifestation-types" class="nav-link">Manif Types</router-link>
+							<router-link v-if="user && user.userRole == 'ADMIN'" to="/customer-types" class="nav-link">Cust Types</router-link>
+						</div>
+					</li>
+
+					<li v-if="user && user.userRole == 'SALESMAN'" class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuManif" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						  Manifestations
+						</a>
+						<div class="dropdown-menu dropdown-menu-dark" style="z-index: 150;" aria-labelledby="navbarDropdownMenuManif">
+							<router-link v-if="user && user.userRole == 'SALESMAN'" to="/salesman-add-manif" class="nav-link">Add Manifestation</router-link>
+							<router-link to="/manifestations" class="nav-item nav-link">All Manifestations</router-link>
+							<router-link to="/my-manifestations" v-if="user && user.userRole == 'SALESMAN'" class="nav-item nav-link">My Manifestations</router-link>
+						</div>
+					</li>
+				
+					<li v-if="user && user.userRole == 'SALESMAN'" class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuTickets" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						  Tickets
+						</a>
+						<div class="dropdown-menu dropdown-menu-dark" style="z-index: 150;" aria-labelledby="navbarDropdownMenuTickets">
+							<router-link to="/sold-tickets" v-if="user && user.userRole == 'SALESMAN'" class="nav-item nav-link">Sold Tickets</router-link>
+							<router-link to="/sold-to-users" v-if="user && user.userRole == 'SALESMAN'" class="nav-item nav-link">My Customers</router-link>
+						</div>
+					</li>
+
+
 					<router-link to="/tickets" v-if="user && (user.userRole == 'CUSTOMER' || user.userRole == 'ADMIN')"  class="nav-item nav-link">Tickets</router-link>
 				</div>
 				<div v-if="!user" class="navbar-nav my-2 my-lg-0">
